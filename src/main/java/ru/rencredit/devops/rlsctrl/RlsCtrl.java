913d34444b69c0e1f3dca0517d21ae41c326e97d
Kotenko, Igor [EXT] <ext_ikotenko@rencredit.ru>
2020-02-11 18:33:05 +0300

    test

--- src/main/java/ru/rencredit/devops/rlsctrl/RlsCtrl.java
+++ src/main/java/ru/rencredit/devops/rlsctrl/RlsCtrl.java
@@ -1,14 +1,29 @@
 package ru.rencredit.devops.rlsctrl;
 
+import javax.inject.Inject;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import ru.rencredit.devops.rlsctrl.model.Solution;
 
 public class RlsCtrl {
+    public static RlsCtrl instance = new RlsCtrl();
     public static void main(String[] args) {
+        RlsCtrl.instance.getSolution();
+    }
+
+    @Inject
+    @ConfigProperty(name="solution.path", defaultValue="./")
+    protected String solutionPath;
+
+    public Solution getSolution() {
+
+        /*
         Config config = ConfigProvider.getConfig();
         String solutionPath = config.getOptionalValue("solution.path", String.class).orElse("./");
+        */
         System.out.printf(" Solution path: %s", solutionPath);
-
+        return new Solution();
     }
+
 }
---
+++ src/main/java/ru/rencredit/devops/rlsctrl/model/Solution.java
@@ -0,0 +1,6 @@
+package ru.rencredit.devops.rlsctrl.model;
+
+public class Solution {
+    Service[] systems;
+    Service[] services;
+}
